2eba49a61c69afa48e122dc7a7b4a9719837d43a
NurNeva <nurozcan2019@gmail.com>
2024-03-19 17:00:08 -0400

    Image Gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/style.css';
+
+import './helper/data.js'
+
+import images from './helper/data.js';
 
 function App() {
   return (
+
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>Image Gallery</h1>
+      </div>
+    
+    <div className='image-container'>
+    {images.map(image=>(
+      <div>
+      <div className='internal-container'>
+      <img src= {image.src.large} alt={image.alt}></img>
+      </div>
+      <p>{image.photographer}</p>
+      </div>
+     ))}
+      
     </div>
+  
+    
+    </div>
+
+
   );
 }
 
